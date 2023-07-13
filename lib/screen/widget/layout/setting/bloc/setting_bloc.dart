1866467eaae27a8b0d1d2fad90b87649ad2c4b3e
tranviendai <viendai50@gmail.com>
2023-07-13 20:24:38 +0700

    reponsive

---
+++ lib/screen/widget/layout/setting/bloc/setting_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jzeno_tea/app/config/theme_config.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingCubit extends Cubit<ThemeData> {
+  SettingCubit() : super(AppTheme.lightTheme);
+
+  Future<void> toggleTheme() async {
+    if (state == AppTheme.lightTheme) {
+      emit(AppTheme.blackTheme);
+    } else {
+      emit(AppTheme.lightTheme);
+    }
+  }
+
+  _saveOptions() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('option', true);
+  }
+}
